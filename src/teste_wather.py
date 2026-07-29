d69ace780e6577dfe2959db03671dbe99917d82f
Lucas Pedro <lucaspedro3650@gmail.com>
2026-07-28 21:44:49 -0300

    coleta com a raw

---
+++ src/teste_wather.py
@@ -0,0 +1,6 @@
+import requests
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+resposta = requests.get(url, params={"q": "Recife,BR"}, timeout=10)
+print("Status:", resposta.status_code)
+print(resposta.text)
